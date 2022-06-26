18fb5c1756bd2580fe70e9c0d983370be24f387c
mayankmehtani <mayank.mehtani@gmail.com>
2022-06-26 19:22:27 +0100

    api changes - use 404 shortcut for stock check & raise 404 first for category

--- shopping/items/views.py
+++ shopping/items/views.py
@@ -10,18 +10,13 @@ from rest_framework.response import Response
 # Create your views here.
 @api_view()
 def get_stock_inventory(request, item_name):
-    try:
-        i = Item.objects.get(name=item_name)
-    except Item.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
+    i = get_object_or_404(Item, name=item_name)
     return Response(data={"current_stock": i.current_stock}, status=status.HTTP_200_OK)
 
 @api_view()
 def get_item_by_category(request, item_category):
-    items = Item.objects.filter(primary_category=item_category)
-
     if item_category not in map(lambda x:x[0], Item.PRIMARY_CHOICES):
         return Response(status=status.HTTP_404_NOT_FOUND)
 
+    items = Item.objects.filter(primary_category=item_category)
     return Response(data=ItemSerializer(items, many=True).data, status=status.HTTP_200_OK)
